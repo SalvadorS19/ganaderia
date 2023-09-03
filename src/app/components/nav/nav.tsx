d909cdd2afcde683ea8a1625bfbad5127ef11b77
Salvador Sanabria <salvadorsanabria14@gmail.com>
2023-09-03 18:30:57 -0500

    init

---
+++ src/app/components/nav/nav.tsx
@@ -0,0 +1,37 @@
+import "./nav.css";
+
+export default function Nav() {
+  return (
+    <header className="layout-header">
+        <div className="container">
+            <nav className="navbar">
+                <h1 className="navbar-logo">El Ganado Feliz</h1>
+                <div className="navbar-container">             
+                    <ul className="navbar-items">
+                        <li className="navbar-item">
+                            <a className="navbar-item-link" href="#">
+                                Inicio
+                            </a>
+                        </li>
+                        <li className="navbar-item">
+                            <a className="navbar-item-link" href="#">
+                                Nosotros
+                            </a>
+                        </li>
+                        <li className="navbar-item">
+                            <a className="navbar-item-link" href="#">
+                                Servicios
+                            </a>
+                        </li>
+                        <li className="navbar-item">
+                            <a className="navbar-item-link" href="#">
+                                Contactanos
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import './assets/layout.css'
+import Nav from './components/nav/nav'
+
+export default function Home() {
+  return (
+    <Nav></Nav>
+  )
+}
