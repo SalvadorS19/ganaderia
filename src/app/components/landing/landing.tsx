168df6a7dd27a644402e9c5d734792ca8769bf53
Salvador Sanabria <salvadorsanabria14@gmail.com>
2023-09-03 20:41:31 -0500

    correccion texto banner

--- src/app/components/landing/landing.tsx
+++ src/app/components/landing/landing.tsx
@@ -8,7 +8,11 @@ function Landing(){
             <div className="banner-container-two">
                 <div className="banner-container-info">
                     <h2 className="banner-title">Bienestar que se saborea.</h2>
-                    <p className="banner-text">Lorem ipsum dolor sit amet elit. Provident. fugit odit? Fugit ipsam. Sed ac ex. Nam mauris velit, ac cursus quis, leo. Non quae, fugiat nihil ad, in ligula.</p>
+                    <p className="banner-text">
+                        Nos comprometemos a brindar un entorno enriquecedor y cuidados expertos, 
+                        garantizando la salud y felicidad de nuestros animales. 
+                        Descubre cómo nuestro enfoque dedicado asegura la calidad y sostenibilidad en cada paso.
+                    </p>
                     <a className="btn btn-secondary btn-style mt-5 me-2" href="#discover"> Ver Ganado </a>
                 </div>
             </div>
