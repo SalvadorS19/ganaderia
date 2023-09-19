241fbfef2c70adcac99f2ba15277c5ba49f31afc
Airton Sampayo <aldairton23@gmail.com>
2023-09-19 12:40:57 -0600

    Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,28 @@
-
+import "./dashboard.css"
 export default function Dashboard(){
-    return <s></s>
+    return (
+    <main className="flex">
+        <aside className="dashboard-aside">
+            <ul className="dashboard-modules">
+                <li> <h2 className="dashboard-title">Modulos</h2></li>
+                <li> <h2 className="dashboard-subtitle">Ganaderia</h2></li>
+                <hr />
+                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Registro animales</li>
+                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Control animales</li>
+                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Plan pastoreo</li>
+                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Salud animales</li>
+                <li> <h2 className="dashboard-subtitle">Administrativo</h2></li>
+                <hr />
+                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Registro actividad</li>
+                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Datos rendimiento</li>
+                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Registro trabajadores</li>
+            </ul>
+        </aside>
+        <section className="dashboard-tabview">
+            no
+        </section>
+    </main>
+    
+    )
+    
 }
