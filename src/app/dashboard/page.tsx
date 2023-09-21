26b5d24fd79be2e82c0281deab9ece8dd2395d39
Salvador Sanabria <salvadorsanabria14@gmail.com>
2023-09-21 11:20:28 -0500

    ajuste items dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,17 +12,17 @@ export default function Dashboard(){
                 <section className="dashboard-section">
                     <h2 className="dashboard-subtitle">Ganaderia</h2>
                     <hr className="mb-3"/>
-                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Registro animales</li>
-                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Control animales</li>
-                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Plan pastoreo</li>
-                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Salud animales</li>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>Registro animales</li>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>Control de inventario</li>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>Plan pastoreo</li>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>Salud animales</li>
                 </section>
                 <section className="dashboard-section">
                     <h2 className="dashboard-subtitle">Administrativo</h2>
                     <hr className="mb-3"/>
-                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Registro actividad</li>
-                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Datos rendimiento</li>
-                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Registro trabajadores</li>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>Registro actividad</li>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>Datos rendimiento</li>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>Registro trabajadores</li>
                 </section>
             </ul>
         </aside>
