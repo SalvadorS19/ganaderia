c20f54d85ea78369adfbd6491d4c94b5d283a5be
Salvador Sanabria <salvadorsanabria14@gmail.com>
2023-09-21 10:53:13 -0500

    ajustes sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,18 +4,22 @@ export default function Dashboard(){
     <main className="flex">
         <aside className="dashboard-aside">
             <ul className="dashboard-modules">
-                <li> <h2 className="dashboard-title">Modulos</h2></li>
-                <li> <h2 className="dashboard-subtitle">Ganaderia</h2></li>
-                <hr />
-                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Registro animales</li>
-                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Control animales</li>
-                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Plan pastoreo</li>
-                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Salud animales</li>
-                <li> <h2 className="dashboard-subtitle">Administrativo</h2></li>
-                <hr />
-                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Registro actividad</li>
-                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Datos rendimiento</li>
-                <li className="dashboard-item"> <i className="uil uil-archive"></i>   Registro trabajadores</li>
+                <h2 className="dashboard-title">Modulos</h2>
+                <section className="dashboard-section">
+                    <h2 className="dashboard-subtitle">Ganaderia</h2>
+                    <hr className="mb-3"/>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Registro animales</li>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Control animales</li>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Plan pastoreo</li>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Salud animales</li>
+                </section>
+                <section className="dashboard-section">
+                    <h2 className="dashboard-subtitle">Administrativo</h2>
+                    <hr className="mb-3"/>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Registro actividad</li>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Datos rendimiento</li>
+                    <li className="dashboard-item"> <i className="uil uil-archive"></i>   Registro trabajadores</li>
+                </section>
             </ul>
         </aside>
         <section className="dashboard-tabview">
